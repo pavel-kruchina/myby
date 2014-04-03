9c184e8f1a48a8359f642ba257090fe2460076b6
pavel.kruchina <pavel.kruchina@gmail.com>
2014-04-03 18:46:02 +0300

    db struct

---
+++ DB-loader/sql/2014-04-03_18-34.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `vsevrtes_main`.`event`(		  
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100),
+  `create_date` DATETIME,
+  `processed` ENUM('no','yes') DEFAULT 'no',
+  `data` TEXT,
+  PRIMARY KEY (`id`),
+  INDEX `name` (`name`)
+) ENGINE=INNODB CHARSET=utf8 COLLATE=utf8_general_ci;
